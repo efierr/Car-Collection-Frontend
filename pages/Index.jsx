a9d292aa4b75495b755dd1eac371fddbfd802d4a
efierr <erickfierro0413@gmail.com>
2024-07-22 22:11:16 -0400

    added cars.jsx and car.jsx data

--- pages/Index.jsx
+++ pages/Index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Cars from "../Components/Cars";
 
 function Index() {
-  return <div>Index</div>;
+  return (
+    <div>
+      INDEX
+      <Cars />
+    </div>
+  );
 }
 
 export default Index;
